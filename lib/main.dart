392884557340b83681b5ec1cf0c3bf3888d47111
visal sandeep adikari <122593452+visaladi@users.noreply.github.com>
2024-04-05 11:48:09 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
-import 'Home.dart';
+import 'Home.dart'; // Assuming Home.dart contains your HomePage widget
 
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your App Name',
+      theme: ThemeData(
+        // Define your app's theme
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(), // This should point to your home screen widget
+    );
+  }
+}
+
