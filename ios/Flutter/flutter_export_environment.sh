5340d3dbdd9790cecb9fec54c76f1e01f8a7c6cc
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-04-09 11:35:21 +0530

    Chathura

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_windows_3.13.8-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\visal Adikari\OneDrive\Desktop\uni sem 6\software project\CakeOclock"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Cake O' Clock\CakeOClock Mobile\CakeOClockMobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
